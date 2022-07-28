27908a212cb528dcdd0aba342ea1d21f391acfe7
Dev-Muhammad-Junaid <Dev.MuhammadJunaid@gmail.com>
2022-07-28 17:05:06 +0500

    Spinner Added.

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:world_time/services/world_time.dart';
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -33,10 +34,10 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(70.0),
-        child: Text("Loading",style: TextStyle(fontSize: 30.0,backgroundColor: Colors.deepPurple,color: Colors.amber),),
-      ),
+      backgroundColor: Colors.deepPurple,
+      body: Center(
+        child: SpinKitFadingCircle(color: Colors.white,size: 50.0,duration: Duration(milliseconds: 1000),),
+      )
     );
   }
 }
