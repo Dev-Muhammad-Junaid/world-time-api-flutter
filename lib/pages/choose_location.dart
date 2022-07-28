f5ae29729b422376e64a63d670d85aa7072b32ab
Dev-Muhammad-Junaid <Dev.MuhammadJunaid@gmail.com>
2022-07-29 02:49:44 +0500

    Finalized Version

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -17,7 +17,7 @@ class _ChooseLocationState extends State<ChooseLocation> {
     WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
     WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
     WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
-    WorldTime(url: 'Asia/China', location: 'Jakarta', flag: 'indonesia.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
   ];
 
   void updateTime(index) async {
